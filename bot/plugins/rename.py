cc856f19343e183eb678c4f35fc559b3482e2cda
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-04-08 22:58:00 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -39,9 +39,10 @@ async def rename_handler(c: Client, m: Message):
                                quote=True)
             return
     await add_user_to_database(c, m)
-    if (not m.reply_to_message) or (not m.reply_to_message.media) or (not get_file_attr(m.reply_to_message)):
+    if (not m.reply_to_message):
         return await m.reply_text("Reply to any document/video/audio to rename it!", quote=True)
-
+    ids = (m.reply_to_message.text).split(" ")
+    file_p = await c.get_messages(int("-100" + ids[0]) , int(ids[1]))
     # Proceed
     editable = await m.reply_text("Now send me new file name!", quote=True)
     user_input_msg: Message = await c.listen(m.chat.id)
@@ -51,33 +52,33 @@ async def rename_handler(c: Client, m: Message):
     if user_input_msg.text and user_input_msg.text.startswith("/"):
         await editable.edit("Process Cancelled!")
         return await user_input_msg.continue_propagation()
-    _raw_file_name = get_media_file_name(m.reply_to_message)
+    _raw_file_name = get_media_file_name(file_p)
     if not _raw_file_name:
-        _file_ext = mimetypes.guess_extension(get_file_attr(m.reply_to_message).mime_type)
+        _file_ext = mimetypes.guess_extension(get_file_attr(file_p).mime_type)
         _raw_file_name = "UnknownFileName" + _file_ext
     if user_input_msg.text.rsplit(".", 1)[-1].lower() != _raw_file_name.rsplit(".", 1)[-1].lower():
         file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + _raw_file_name.rsplit(".", 1)[-1].lower()
     else:
         file_name = user_input_msg.text[:255]
     await editable.edit("Please Wait ...")
-    is_big = get_media_file_size(m.reply_to_message) > (10 * 1024 * 1024)
+    is_big = get_media_file_size(file_p) > (10 * 1024 * 1024)
     if not is_big:
         _default_thumb_ = await db.get_thumbnail(m.from_user.id)
         if not _default_thumb_:
-            _m_attr = get_file_attr(m.reply_to_message)
+            _m_attr = get_file_attr(file_p)
             _default_thumb_ = _m_attr.thumbs[0].file_id \
                 if (_m_attr and _m_attr.thumbs) \
                 else None
-        await handle_not_big(c, m, get_media_file_id(m.reply_to_message), file_name,
-                             editable, get_file_type(m.reply_to_message), _default_thumb_)
+        await handle_not_big(c, m, get_media_file_id(file_p), file_name,
+                             editable, get_file_type(file_p), _default_thumb_)
         return
-    file_type = get_file_type(m.reply_to_message)
-    _c_file_id = FileId.decode(get_media_file_id(m.reply_to_message))
+    file_type = get_file_type(file_p)
+    _c_file_id = FileId.decode(get_media_file_id(file_p))
     try:
         c_time = time.time()
         file_id = await c.custom_upload(
             file_id=_c_file_id,
-            file_size=get_media_file_size(m.reply_to_message),
+            file_size=get_media_file_size(file_p),
             file_name=file_name,
             progress=progress_for_pyrogram,
             progress_args=(
